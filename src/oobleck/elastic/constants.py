d0f63bb53cbfbb12209c66b0b3f5bcc451220293
Insu Jang <insujang@umich.edu>
2023-03-27 05:47:54 +0000

    elastic: remove unused code

--- src/oobleck/elastic/constants.py
+++
@@ -1,5 +0,0 @@
-OOBLECK_RECONFIGURATION = "oobleck/reconfiguration"
-OOBLECK_WORLD_INFO = "oobleck/world_info"
-OOBLECK_TOPOLOGY_INFO = "oobleck/topology_info"
-OOBLECK_TRAINING_IN_PROGRESS = "oobleck/training_in_progress"
-OOBLECK_TORCH_MASTER_INFO = "oobleck/torch_master_info"
