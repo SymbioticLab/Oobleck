730226cacd3c8ae3cb1eb8e13449859d384d1120
Insu Jang <insujang@umich.edu>
2023-04-02 16:24:01 +0000

    Add model examples

---
+++ examples/vit.py
@@ -0,0 +1,15 @@
+import rpyc
+
+ft_spec = 0
+model_name = "google/vit-large-patch16-224"
+dataset_path = "Maysee/tiny-imagenet"
+dataset_name = None
+training_args = {
+    "per_device_train_batch_size": 16,
+    "max_steps": 30,
+}
+
+client = rpyc.connect("localhost", 27322)
+client.root.run_model(
+    ft_spec, model_name, dataset_path, dataset_name, None, training_args
+)
