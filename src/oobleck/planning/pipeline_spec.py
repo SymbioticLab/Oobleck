Fix bug in calculating number of microbatch calculation algorithm

src/oobleck/planning/pipeline_spec.py
@@ -51,7 +51,7 @@ class StageExecutionResult:
     @property
     def memory_consumption(self) -> int:
         # TODO: consider activation as well.
-        return self.mem_required / self.device_num
+        return self.mem_required / self.device_num * 6
 
     def __repr__(self) -> str:
         return (
@@ -70,7 +70,7 @@ class DCExecutionResult:
         self.stages = stages
 
         if len(stages) == 0 or any(
-            stage.memory_consumption / stage.device_num
+            stage.memory_consumption
             > torch.cuda.get_device_properties("cuda:0").total_memory
             for stage in stages
         ):
@@ -312,7 +312,13 @@ class PipelineSpec:
     as a linear of combination of PipelineSpecs with consecutive number of nodes.
     """
 
-    def __init__(self, num_nodes: int, num_gpus_per_node: int, model: OobleckModel, microbatch_size: int):
+    def __init__(
+        self,
+        num_nodes: int,
+        num_gpus_per_node: int,
+        model: OobleckModel,
+        microbatch_size: int,
+    ):
         assert (
             num_nodes > 0 and num_gpus_per_node > 0
         ), f"Number of nodes or GPUs cannot be 0 or negative - # nodes: {num_nodes}, # GPUs: {num_gpus_per_node}"
@@ -401,7 +407,7 @@ class PipelineSpec:
 
         # TODO: currently required memory calculation is not correct.
         model_layers = get_profile_results(model, microbatch_size)
-        required_memory = sum(layer.mem_required for layer in model_layers) * 6 * 1.2
+        required_memory = sum(layer.mem_required for layer in model_layers) * 6
         # required_memory = model.total_num_params * 12 * 4
         gpu_memory = torch.cuda.get_device_properties("cuda:0").total_memory
         required_min_gpus = math.ceil(required_memory / gpu_memory)