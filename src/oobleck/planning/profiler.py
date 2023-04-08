fea0c02188b9ea62bdd650eba8a45edd0e0ffa93
Insu Jang <insujang@umich.edu>
2023-04-08 14:34:06 +0000

    profiler: share model profile data across mb execution latency

--- src/oobleck/planning/profiler.py
+++ src/oobleck/planning/profiler.py
@@ -46,7 +46,9 @@ class LayerExecutionResult:
         )
 
 
-def get_profile_results(model: OobleckModel, microbatch_size: int) -> List[LayerExecutionResult]:
+def get_profile_results(
+    model: OobleckModel, microbatch_size: int
+) -> List[LayerExecutionResult]:
     """Get the profiling results.
 
     Returns:
@@ -65,8 +67,8 @@ def get_profile_results(model: OobleckModel, microbatch_size: int) -> List[Layer
     directory = f"{PROFILE_CACHE}/{model.model_name}-{model.model_tag}"
 
     layer_execution_result = get_cache(f"{directory}/mb{microbatch_size}/layers")
-    allreduce_across_nodes = get_cache(f"{directory}/mb{microbatch_size}/allreduce_across_nodes")
-    allreduce_in_node = get_cache(f"{directory}/mb{microbatch_size}/allreduce_in_node")
+    allreduce_across_nodes = get_cache(f"{directory}/allreduce_across_nodes")
+    allreduce_in_node = get_cache(f"{directory}/allreduce_in_node")
 
     results: List[LayerExecutionResult] = []
     for layer, execution, ar_in_node, ar_across_nodes in zip(
@@ -110,9 +112,16 @@ class Profiler:
         if dist.get_rank() == 0:
             for i in range(num_warmup + num_iteration):
                 logger.info(f"Profiling layer execution ltency: {i} iteration")
-                input = tuple([t.detach().clone().to("cuda") for t in self.model.sample_inputs.values()])
+                input = tuple(
+                    [
+                        t.detach().clone().to("cuda")
+                        for t in self.model.sample_inputs.values()
+                    ]
+                )
 
-                input = tuple(torch.stack([input[i]] * batch_size) for i in range(len(input)))
+                input = tuple(
+                    torch.stack([input[i]] * batch_size) for i in range(len(input))
+                )
 
                 for idx, layer in enumerate(self.model.model):
                     start_mem = torch.cuda.memory_allocated()
@@ -131,9 +140,7 @@ class Profiler:
                     if isinstance(output, tuple):
                         input = tuple(
                             [
-                                t.detach().clone()
-                                if isinstance(t, torch.Tensor)
-                                else t
+                                t.detach().clone() if isinstance(t, torch.Tensor) else t
                                 for t in output
                             ]
                         )
@@ -315,9 +322,6 @@ def profile(
     os.environ["LOCAL_RANK"] = str(local_rank)
 
     directory = f"{PROFILE_CACHE}/{model_name}-{model_tag}"
-    if Path(f"{directory}/mb{microbatch_size}").exists():
-        logger.info("Profile results already exist. Skipping profiling.")
-        return
 
     # assert dist.is_initialized(), "Distributed is not initialized."
     assert not dist.is_initialized(), "Distributed is already initialized."
@@ -329,21 +333,39 @@ def profile(
     model = OobleckModel(model_name, sample_inputs, None, model_tag, model_args)
     profiler = Profiler(model)
     # forward/backward execution
-    layer_execution_result = profiler.profile_execution_layers(microbatch_size)
-    allreduce_across_nodes = profiler.profile_allreduce_across_nodes()
-    allreduce_in_node = profiler.profile_allreduce_in_node()
 
-    if "0" in os.environ["CUDA_VISIBLE_DEVICES"]:
-        with Path(f"{directory}/mb{microbatch_size}/layers").open(mode="w") as f:
-            f.write(str(layer_execution_result))
-            f.flush()
-        with Path(f"{directory}/mb{microbatch_size}/allreduce_across_nodes").open(mode="w") as f:
-            f.write(str(allreduce_across_nodes))
-            f.flush()
-        with Path(f"{directory}/mb{microbatch_size}/allreduce_in_node").open(mode="w") as f:
-            f.write(str(allreduce_in_node))
-            f.flush()
-        logger.info("Profile data is stored in %s", directory)
+    path = f"{directory}/mb{microbatch_size}/layers"
+    if Path(path).exists():
+        logger.info("Skip profiling execution latency.")
+    else:
+        logger.info("Profiling model execution latency.")
+        layer_execution_result = profiler.profile_execution_layers(microbatch_size)
+        if "0" in os.environ["CUDA_VISIBLE_DEVICES"]:
+            with Path(path).open(mode="w") as f:
+                f.write(str(layer_execution_result))
+                f.flush()
+
+    path = f"{directory}/allreduce_across_nodes"
+    if Path(path).exists():
+        logger.info("Skip profiling cross-node allreduce latency.")
+    else:
+        logger.info("Profiling cross-node allreduce latency.")
+        allreduce_across_nodes = profiler.profile_allreduce_across_nodes()
+        if "0" in os.environ["CUDA_VISIBLE_DEVICES"]:
+            with Path(path).open(mode="w") as f:
+                f.write(str(allreduce_across_nodes))
+                f.flush()
+
+    path = f"{directory}/allreduce_in_node"
+    if Path(path).exists():
+        logger.info("Skip profiling in-node allreduce latency.")
+    else:
+        logger.info("Profiling in-node allreduce latency.")
+        allreduce_in_node = profiler.profile_allreduce_in_node()
+        if "0" in os.environ["CUDA_VISIBLE_DEVICES"]:
+            with Path(path).open(mode="w") as f:
+                f.write(str(allreduce_in_node))
+                f.flush()
 
     dist.barrier()
     dist.destroy_process_group()
