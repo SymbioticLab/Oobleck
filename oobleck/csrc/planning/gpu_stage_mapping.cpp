8aac9d58fdaa2f11bce23175ee7f44953c3a36d9
Insu Jang <insujang@umich.edu>
2023-04-29 23:34:14 +0000

    submodule update

--- oobleck/csrc/planning/gpu_stage_mapping.cpp
+++ oobleck/csrc/planning/gpu_stage_mapping.cpp
@@ -1,10 +1,14 @@
 #include <concurrentqueue.h>
+#include <parallel_hashmap/phmap.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <BS_thread_pool.hpp>
+#include <cassert>
 #include <nlohmann/json.hpp>
 #include "execution_result.h"
+#include "pipeline_template.h"
 
+namespace py = pybind11;
 /**
  * Section 4.1.2. GPU-Stage Mapping using divide and conquer algorithm.
  * The divide and conquer is accelerated using multithreading
@@ -13,15 +17,39 @@
 
 namespace oobleck {
 
-int test() {
-  return 0;
+using LayerIndex = int;
+
+static moodycamel::ConcurrentQueue<DCExecutionResult> divide_queue;
+static moodycamel::ConcurrentQueue<DCExecutionResult> conquer_queue;
+static moodycamel::ConcurrentQueue<DCExecutionResult> combine_queue;
+static map<LayerIndex, LayerExecutionResult> layer_execution_results;
+
+void push_task(map<DCExecutionResult::key, double>& cache,
+               int num_stages,
+               int layer_start_index,
+               int layer_end_index,
+               int num_nodes,
+               int num_gpus_per_node) {
+  // base case
+  if (num_stages == 1) {
+    assert(num_nodes == 1);
+  }
+}
+
+std::vector<PipelineTemplate> create_pipeline_templates(
+    int f,
+    int num_nodes,
+    int num_gpus_per_node,
+    const std::string& model_name,
+    int microbatch_size) {
+  // 1. Create a cache, where the key is the DCExecutionResult::key
+  // and the value is the execution time.
+  map<DCExecutionResult::key, double> cache;
 }
 
 }  // namespace oobleck
 
-namespace py = pybind11;
-
-PYBIND11_MODULE(cplanning, m) {
-  m.doc() = "Oobleck planning module";
-  m.def("test", &oobleck::test, "Test function");
-}
+// PYBIND11_MODULE(cplanning, m) {
+//   m.doc() = "Oobleck planning module";
+//   m.def("test", &oobleck::test, "Test function");
+// }
