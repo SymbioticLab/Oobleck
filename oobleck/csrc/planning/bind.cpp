f6762811d7c73b46994cbc56723d2954aea37c77
Insu Jang <insujang@umich.edu>
2023-05-25 14:32:18 +0000

    Change pybind to expose stageexecutionresults

--- oobleck/csrc/planning/bind.cpp
+++ oobleck/csrc/planning/bind.cpp
@@ -7,16 +7,18 @@ namespace py = pybind11;
 PYBIND11_MODULE(pipeline_template, m) {
   m.doc() = "Oobleck pipeline template module";
 
-  py::class_<oobleck::StageExecutionResult>(m, "StageExecutionResult");
+  py::class_<oobleck::StageExecutionResult,
+             std::shared_ptr<oobleck::StageExecutionResult>>(
+      m, "StageExecutionResult");
 
-  py::class_<oobleck::PipelineTemplate>(m, "PipelineTemplate");
+  py::class_<oobleck::PipelineTemplate>(m, "PipelineTemplate")
+      .def("get_stage_execution_results",
+           &oobleck::PipelineTemplate::get_stage_execution_results);
   //  .def(
   //      py::init<std::vector<std::shared_ptr<oobleck::StageExecutionResult>>&,
   //               int, int, int>(),
   //      py::arg("stage_execution_results"), py::arg("num_layers"),
   //      py::arg("num_nodes"), py::arg("num_gpus_per_node"))
-  //  .def("get_stage_execution_results",
-  //       &oobleck::PipelineTemplate::get_stage_execution_results)
   //  .def("get_num_nodes", &oobleck::PipelineTemplate::get_num_nodes)
   //  .def("get_num_gpus_per_node",
   //       &oobleck::PipelineTemplate::get_num_gpus_per_node);
