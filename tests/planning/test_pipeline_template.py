67fc6a9861599533acd7ac8cad86a1b390f90f37
Insu Jang <insujang@umich.edu>
2023-06-07 04:26:33 +0000

    use dummy profile for pipeline template unittest

--- tests/planning/test_pipeline_template.py
+++ tests/planning/test_pipeline_template.py
@@ -1,11 +1,53 @@
 import pytest
+import random
+import json
+from pathlib import Path
+import shutil
 
 
-@pytest.mark.order(after="test_profiler.py::test_profile")
-def test_create_pipeline_templates_onenode(gpt2_model, pipeline_template_generator):
+@pytest.fixture(scope="module")
+def dummy_model():
+    model_name = "testmodel"
+    model_tag = "dev"
+    num_layers = 32
+
+    layers = []
+    allreduce_acorss_nodes = []
+    allreduce_in_node = []
+    for _ in range(num_layers):
+        layers.append(
+            {
+                "forward": random.random(),
+                "backward": random.random() * 3,
+                "mem_required": [1024.0, 1024.0],
+            }
+        )
+        allreduce_acorss_nodes.append(
+            {"1": random.random() * 4, "4": random.random() * 4}
+        )
+        allreduce_in_node.append({"1": random.random(), "4": random.random()})
+
+    directory = Path(f"/tmp/oobleck/profiles/{model_name}-{model_tag}")
+    directory.mkdir(parents=True, exist_ok=True)
+
+    with directory.joinpath("mb1.json").open("w") as f:
+        json.dump(layers, f)
+    with directory.joinpath("mb4.json").open("w") as f:
+        json.dump(layers, f)
+    with directory.joinpath("allreduce_in_node.json").open("w") as f:
+        json.dump(allreduce_in_node, f)
+    with directory.joinpath("allreduce_across_nodes.json").open("w") as f:
+        json.dump(allreduce_acorss_nodes, f)
+
+    yield model_name, model_tag, num_layers
+
+    shutil.rmtree(directory)
+
+
+def test_create_pipeline_templates_onenode(dummy_model, pipeline_template_generator):
     pipeline_templates = pipeline_template_generator.create_pipeline_templates(
-        gpt2_model.model_name,
-        gpt2_model.model_tag,
+        dummy_model[0],
+        dummy_model[1],
         1,  # microbatch_size
         (1, 1),  # num nodes range
         1,
@@ -17,12 +59,11 @@ def test_create_pipeline_templates_onenode(gpt2_model, pipeline_template_generat
     assert pipeline_templates[0].get_iteration_time() > 0
 
 
-@pytest.mark.order(after="test_profiler.py::test_profile")
-def test_create_pipeline_templates_maxnode(gpt2_model, pipeline_template_generator):
-    num_nodes = len(gpt2_model.model)
+def test_create_pipeline_templates_maxnode(dummy_model, pipeline_template_generator):
+    num_nodes = dummy_model[2]
     pipeline_templates = pipeline_template_generator.create_pipeline_templates(
-        gpt2_model.model_name,
-        gpt2_model.model_tag,
+        dummy_model[0],
+        dummy_model[1],
         1,  # microbatch_size
         (num_nodes, num_nodes),  # num nodes range
         1,
@@ -34,14 +75,13 @@ def test_create_pipeline_templates_maxnode(gpt2_model, pipeline_template_generat
     assert pipeline_templates[0].get_iteration_time() > 0
 
 
-@pytest.mark.order(after="test_profiler.py::test_profile")
 def test_create_pipeline_templates_toomanynodes(
-    gpt2_model, pipeline_template_generator
+    dummy_model, pipeline_template_generator
 ):
-    num_nodes = len(gpt2_model.model) + 1
+    num_nodes = dummy_model[2] + 1
     pipeline_templates = pipeline_template_generator.create_pipeline_templates(
-        gpt2_model.model_name,
-        gpt2_model.model_tag,
+        dummy_model[0],
+        dummy_model[1],
         1,  # microbatch_size
         (num_nodes, num_nodes),  # num nodes range
         1,
@@ -49,44 +89,57 @@ def test_create_pipeline_templates_toomanynodes(
     assert len(pipeline_templates) == 0
 
 
-@pytest.mark.order(after="test_profiler.py::test_profile")
-def test_create_pipeline_templates_noderange(gpt2_model, pipeline_template_generator):
+def test_create_pipeline_templates_noderange(dummy_model, pipeline_template_generator):
+    num_nodes = dummy_model[2]
     pipeline_templates = pipeline_template_generator.create_pipeline_templates(
-        gpt2_model.model_name,
-        gpt2_model.model_tag,
+        dummy_model[0],
+        dummy_model[1],
         1,  # microbatch_size
-        (1, len(gpt2_model.model)),  # num nodes range
+        (1, dummy_model[2]),  # num nodes range
         1,
     )
-    assert 0 < len(pipeline_templates) <= len(gpt2_model.model)
-    assert 0 < pipeline_templates[0].get_num_nodes() <= len(gpt2_model.model)
+    assert 0 < len(pipeline_templates) <= num_nodes
+    assert 0 < pipeline_templates[0].get_num_nodes() <= num_nodes
     assert len(pipeline_templates[0].get_stages()) == 1
-    assert len(pipeline_templates[-1].get_stages()) == len(gpt2_model.model)
+    assert len(pipeline_templates[-1].get_stages()) == num_nodes
     for pipeline_template in pipeline_templates:
         assert pipeline_templates[0].get_num_gpus_per_node() == 1
         assert len(pipeline_template.get_stages()) > 0
         assert pipeline_template.get_iteration_time() > 0
 
 
-@pytest.mark.order(after="test_profiler.py::test_profile_multimicrobatch")
 def test_create_pipeline_templates_multimicrobatch(
-    gpt2_model, pipeline_template_generator
+    dummy_model, pipeline_template_generator
 ):
+    num_nodes = dummy_model[2]
     pipeline_templates = pipeline_template_generator.create_pipeline_templates(
-        gpt2_model.model_name,
-        gpt2_model.model_tag,
+        dummy_model[0],
+        dummy_model[1],
         4,  # microbatch_size
-        (1, len(gpt2_model.model)),  # num nodes range
+        (1, num_nodes),  # num nodes range
         1,
     )
-    assert 0 < len(pipeline_templates) <= len(gpt2_model.model)
-    assert 0 < pipeline_templates[0].get_num_nodes() <= len(gpt2_model.model)
+    assert 0 < len(pipeline_templates) <= num_nodes
+    assert 0 < pipeline_templates[0].get_num_nodes() <= num_nodes
     assert len(pipeline_templates[0].get_stages()) == 1
-    assert len(pipeline_templates[-1].get_stages()) == len(gpt2_model.model)
+    assert len(pipeline_templates[-1].get_stages()) == num_nodes
     for pipeline_template in pipeline_templates:
         assert pipeline_templates[0].get_num_gpus_per_node() == 1
         assert len(pipeline_template.get_stages()) > 0
         assert pipeline_template.get_iteration_time() > 0
+
+
+def test_create_pipeline_templates_no_microbatch_profile(
+    dummy_model, pipeline_template_generator
+):
+    with pytest.raises(Exception):
+        pipeline_template_generator.create_pipeline_templates(
+            dummy_model[0],
+            dummy_model[1],
+            2,  # microbatch_size
+            (1, 1),  # num nodes range
+            1,
+        )
 
 
 @pytest.mark.skip(reason="TODO")
