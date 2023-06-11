2aa6ab2dcbcadea91fa785f1736442f6efb72f06
Insu Jang <insujang@umich.edu>
2023-06-11 04:12:27 +0000

    remove unnecessary unittests

--- tests/planning/test_pipeline_template.py
+++ tests/planning/test_pipeline_template.py
@@ -73,43 +73,6 @@ def test_create_pipeline_templates_noderange(
 
 
 @pytest.mark.skip(reason="TODO")
-def test_create_pipeline_templates_multimicrobatch(
-    dummy_layer_execution_results: LayerExecutionResults,
-):
-    generator = PipelineTemplateGenerator()
-    num_nodes = dummy_layer_execution_results._size
-    pipeline_templates = generator.create_pipeline_templates(
-        dummy_layer_execution_results,
-        (1, num_nodes),  # num nodes range
-        1,
-    )
-    assert 0 < len(pipeline_templates) <= num_nodes
-    assert 0 < pipeline_templates[0]._num_nodes <= num_nodes
-    assert len(pipeline_templates[0]._stages) == 1
-    assert len(pipeline_templates[-1]._stages) == num_nodes
-    for pipeline_template in pipeline_templates:
-        assert pipeline_templates[0]._num_gpus_per_node == 1
-        assert len(pipeline_template._stages) > 0
-        assert pipeline_template._iteration_time > 0
-
-
-@pytest.mark.skip(reason="TODO")
-def test_create_pipeline_templates_no_microbatch_profile(
-    dummy_layer_execution_results: LayerExecutionResults,
-):
-    assert False
-    generator = PipelineTemplateGenerator()
-    with pytest.raises(Exception):
-        generator.create_pipeline_templates(
-            dummy_model[0],
-            dummy_model[1],
-            2,  # microbatch_size
-            (1, 1),  # num nodes range
-            1,
-        )
-
-
-@pytest.mark.skip(reason="TODO")
 def test_create_pipeline_templates_fsdp(model):
     assert False
     generator = PipelineTemplateGenerator()
