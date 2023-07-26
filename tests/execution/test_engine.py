2621828d4bd9aecdfef9e5d8069aee3144e5df75
Insu Jang <insujang@umich.edu>
2023-07-26 06:33:37 +0000

    code cleanup

--- tests/execution/test_engine.py
+++ tests/execution/test_engine.py
@@ -598,8 +598,6 @@ class TestOobleckReconfigurationClass(OobleckSingleProcessTestCase):
         ):
             assert pipeline._ranks == expected_rank
 
-    [0, 1], [2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12, 13]
-
     @pytest.mark.parametrize(
         ["failed_ranks", "expected_ranks"],
         [
