54bbb7354140c739de07d8c4ef9828d1a55286f4
Insu Jang <insujang@umich.edu>
2023-09-01 06:26:49 +0000

    bugfix in accessing global timer

--- oobleck/utils/timer.py
+++ oobleck/utils/timer.py
@@ -2,14 +2,14 @@ from deepspeed.utils.logging import LoggerFactory
 from deepspeed.utils.timer import SynchronizedWallClockTimer
 
 logger = LoggerFactory.create_logger(__name__)
-timer = SynchronizedWallClockTimer()
+sync_timer = SynchronizedWallClockTimer()
 
 
 def measure_time(timer_name: str):
     def inner(func: callable):
         def wrapper(s, *args, **kwargs):
-            global timer
-            timer: SynchronizedWallClockTimer.Timer = timer.timer(timer_name)
+            global sync_timer
+            timer: SynchronizedWallClockTimer.Timer = sync_timer.timer(timer_name)
             timer.start()
             # TODO: restore timer later.
             result = func(s, *args, **kwargs)
