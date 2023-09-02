32b0278114efb9a14a8bd83ce8c486471c9be263
Insu Jang <insujang@umich.edu>
2023-09-02 05:19:45 +0000

    new arguments dataclasses

--- oobleck/elastic/training_util.py
+++ oobleck/elastic/training_util.py
@@ -1,65 +1,53 @@
-from dataclasses import asdict, dataclass, is_dataclass
+from dataclasses import dataclass, fields, is_dataclass
 
 from simple_parsing import Serializable
 
 
 @dataclass
-class OobleckArguments(Serializable):
-    """Data class that contains information about the training job.
-    OobleckAgent -> OobleckEngine
-    """
+class DistributedArguments(Serializable):
+    master_ip: str
+    master_port: int
+    node_ips: list[str]
+    node_port: int = 22
+    num_workers: int = 0
+    num_agents_per_node: int = 1
+    username: str | None = None
+
+
+@dataclass
+class JobArguments(Serializable):
+    fault_threshold: int = 3
+    microbatch_size: int = 1
+    global_microbatch_size: int = 128
+    steps: int = 50
+
+
+@dataclass
+class ModelArguments(Serializable):
+    def __init__(
+        self,
+        model_name: str,
+        model_tag: str,
+        dataset_path: str,
+        dataset_name: str | None = None,
+        **kwargs,
+    ):
+        self.model_name = model_name
+        self.model_tag = model_tag
+        self.dataset_path = dataset_path
+        self.dataset_name = dataset_name
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     model_name: str
     model_tag: str
     dataset_path: str
     dataset_name: str | None = None
-    fault_threshold: int = 3
-    microbatch_size: int = 1
-    global_microbatch_size: int = 128
-    model_args: dict[str, any] | None = None
-    steps: int = 50
+    # Other arbitrary model configuration can be here..
 
 
 @dataclass
-class OobleckAgentArguments(Serializable):
-    """Data class that contains information about the training job.
-    OobleckMasterDaemon -> OobleckAgent
-    """
-
-    master_ip: str
-    master_port: int
-    node_ips: list[str]
-    job_args: OobleckArguments
-    num_workers: int
-
-
-@dataclass
-class DistributedJobConfiguration(Serializable):
-    """Data class that contains information about distributed training.
-    run.py -> OobleckMasterDaemon
-    """
-
-    master_ip: str
-    master_port: int
-    node_ips: list[str]
-    job_args: OobleckArguments
-    node_port: int = 22
-    username: str | None = None
-
-
-def flatten_configurations(
-    dataclass: OobleckAgentArguments | DistributedJobConfiguration,
-) -> dict:
-    """Flatten the configuration dataclass into a dict."""
-    result = {}
-    for k, v in asdict(dataclass).items():
-        if is_dataclass(v):
-            result.update(flatten_configurations(v))
-        elif isinstance(v, dict):
-            v = {k: v for k, v in v.items() if v is not None}
-            result.update(v)
-        elif isinstance(v, list):
-            result[k] = " ".join(v)
-        else:
-            result[k] = v
-    return result
+class OobleckArguments(Serializable):
+    dist: DistributedArguments
+    job: JobArguments
+    model: ModelArguments
