79bcd3aebbc56c7c92265ddcce8d340e58e80f93
Insu Jang <insujang@umich.edu>
2023-12-21 06:01:43 +0000

    move files to legacy

---
+++ oobleck/legacy/csrc/modules/cppcoro/include/cppcoro/inline_scheduler.hpp
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (c) Lewis Baker
+// Licenced under MIT license. See LICENSE.txt for details.
+///////////////////////////////////////////////////////////////////////////////
+#ifndef CPPCORO_INLINE_SCHEDULER_HPP_INCLUDED
+#define CPPCORO_INLINE_SCHEDULER_HPP_INCLUDED
+
+#include <coroutine>
+
+namespace cppcoro
+{
+	class inline_scheduler
+	{
+	public:
+		inline_scheduler() noexcept = default;
+
+		std::suspend_never schedule() const noexcept { return {}; }
+	};
+}  // namespace cppcoro
+
+#endif
