9220bcf949b18265bc1c2461382ec983441c8fa1
Insu Jang <insujang@umich.edu>
2024-01-05 01:59:51 +0000

    fix test case

--- tests/engine/test_instantiator.py
+++ tests/engine/test_instantiator.py
@@ -6,14 +6,10 @@ from oobleck_colossalai.pipeline_template import PipelineTemplate
 from conftest import homogeneous_templates, heterogeneous_templates
 
 
-@pytest.mark.parametrize(
-    "templates",
-    [homogeneous_templates, heterogeneous_templates],
-    ids=["homogeneous", "heterogeneous"],
-)
-def test_instantiate(templates: dict[PipelineTemplate, int]):
-    instantiator = PipelineInstantiator(list(templates.keys()), 512)
-    result = instantiator.instantiate(6)
+@pytest.mark.parametrize("num_nodes", list(range(4, 17)))
+def test_instantiate(num_nodes: int):
+    instantiator = PipelineInstantiator(list(heterogeneous_templates.keys()), 512)
+    result = instantiator.instantiate(num_nodes)
 
     assert isinstance(result, tuple)
     assert isinstance(result[0], dict)
@@ -27,4 +23,4 @@ def test_instantiate(templates: dict[PipelineTemplate, int]):
         template.num_stages * num_pipelines
         for template, num_pipelines in result[0].items()
     )
-    assert total_num_nodes == 6
+    assert total_num_nodes == num_nodes
