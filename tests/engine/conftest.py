f70b170bfc4912c3de92683117ddae4ab4b499a0
Insu Jang <insujang@umich.edu>
2024-03-22 01:16:19 +0000

    move data builder

--- tests/engine/conftest.py
+++ tests/engine/conftest.py
@@ -1,14 +1,26 @@
 import csv
 from pathlib import Path
 
-import datasets
-import pytest
-from oobleck_colossalai import HeterogeneousParallelPlugin
+from data_builder import GLUEDataBuilder
 from oobleck_colossalai.pipeline_template import PipelineTemplate
-from torch.utils.data import DataLoader
-from transformers import AutoTokenizer, PreTrainedTokenizer
+from oobleck_colossalai.shardformer.policies.gpt2 import (
+    GPT2Config,
+    GPT2ForSequenceClassificationPolicy,
+)
 
+config: GPT2Config = GPT2Config.from_pretrained("gpt2")
+config.is_decoder = True
+config.n_layers = 4
+config.num_labels = GLUEDataBuilder.glue_task_num_labels["mrpc"]
+
+modules: list[str] = GPT2ForSequenceClassificationPolicy.get_all_modules(config)
 model_name: str = "transformers.models.gpt2.modeling_gpt2.GPT2ForSequenceClassification"
+
+template_1stage = PipelineTemplate(model_name, [modules])
+template_2stages = PipelineTemplate(model_name, [modules[:3], modules[3:]])
+template_3stages = PipelineTemplate(
+    model_name, [modules[:4], modules[4:7], modules[7:]]
+)
 
 
 def init_profile_data(file_path: Path):
@@ -39,132 +51,3 @@ def init_profile_data(file_path: Path):
                     "mem_required": 10,
                 }
             )
-
-
-template_1stage = PipelineTemplate(
-    model_name,
-    [
-        [
-            "transformer.wte",
-            "transformer.wpe",
-            "transformer.drop",
-            *[f"transformer.h.{i}" for i in range(0, 4)],
-            "transformer.ln_f",
-            "score",
-        ]
-    ],
-)
-
-template_2stages = PipelineTemplate(
-    model_name,
-    [
-        ["transformer.wte", "transformer.wpe", "transformer.drop"],
-        [*[f"transformer.h.{i}" for i in range(0, 4)], "transformer.ln_f", "score"],
-    ],
-)
-
-template_3stages = PipelineTemplate(
-    model_name,
-    [
-        [
-            "transformer.wte",
-            "transformer.wpe",
-            "transformer.drop",
-            "transformer.h.0",
-        ],
-        [f"transformer.h.{i}" for i in range(1, 4)],
-        ["transformer.ln_f", "score"],
-    ],
-)
-
-
-class GLUEDataBuilder:
-    task_text_field_map = {
-        "cola": ["sentence"],
-        "sst2": ["sentence"],
-        "mrpc": ["sentence1", "sentence2"],
-        "qqp": ["question1", "question2"],
-        "stsb": ["sentence1", "sentence2"],
-        "mnli": ["premise", "hypothesis"],
-        "qnli": ["question", "sentence"],
-        "rte": ["sentence1", "sentence2"],
-        "wnli": ["sentence1", "sentence2"],
-        "ax": ["premise", "hypothesis"],
-    }
-
-    loader_columns = [
-        "datasets_idx",
-        "input_ids",
-        "token_type_ids",
-        "attention_mask",
-        "start_positions",
-        "end_positions",
-        "labels",
-    ]
-
-    def __init__(
-        self,
-        model_name_or_path: str,
-        plugin: HeterogeneousParallelPlugin,
-        task_name: str = "mrpc",
-        max_seq_length: int = 128,
-    ):
-        self.plugin = plugin
-        self.tokenizer: PreTrainedTokenizer = AutoTokenizer.from_pretrained(
-            model_name_or_path, use_fast=True
-        )
-        self.tokenizer.pad_token = self.tokenizer.eos_token
-        self.dataset = datasets.load_dataset("glue", task_name)
-
-        def convert_to_features(example_batch):
-            text_fields = GLUEDataBuilder.task_text_field_map[task_name]
-            if len(text_fields) > 1:
-                texts_or_text_pairs = list(
-                    zip(example_batch[text_fields[0]], example_batch[text_fields[1]])
-                )
-            else:
-                texts_or_text_pairs = example_batch[text_fields[0]]
-
-            # Tokenize the text/text pairs
-            features = self.tokenizer.batch_encode_plus(
-                texts_or_text_pairs,
-                max_length=max_seq_length,
-                padding="max_length",
-                truncation=True,
-            )
-
-            features["labels"] = example_batch["label"]
-            return features
-
-        for split in self.dataset.keys():
-            self.dataset[split] = self.dataset[split].map(
-                convert_to_features,
-                batched=True,
-                remove_columns=["label"],
-            )
-            self.columns = [
-                c
-                for c in self.dataset[split].column_names
-                if c in GLUEDataBuilder.loader_columns
-            ]
-            self.dataset[split].set_format(type="torch", columns=self.columns)
-
-    def dataloader(self) -> DataLoader:
-        return self.plugin.prepare_dataloader(
-            self.dataset["train"],
-            shuffle=True,
-            drop_last=True,
-        )
-
-
-@pytest.fixture(
-    scope="class",
-    params=[
-        [template_3stages, template_3stages, template_3stages],
-        [template_3stages, template_2stages, template_2stages],
-    ],
-)
-def plugin(request: pytest.FixtureRequest):
-    plugin = HeterogeneousParallelPlugin(tp_size=2, microbatch_size=1)
-    plugin.set_pipelines(request.param)
-    request.cls.plugin = plugin
---
+++ tests/engine/data_builder.py
@@ -0,0 +1,117 @@
+# Coda copied from colossalai/examples/language/gpt/hybridparallelism/data.py
+
+import datasets
+from colossalai.booster.plugin.dp_plugin_base import DPPluginBase
+from transformers import AutoTokenizer, PreTrainedTokenizer
+
+
+class GLUEDataBuilder:
+    task_text_field_map = {
+        "cola": ["sentence"],
+        "sst2": ["sentence"],
+        "mrpc": ["sentence1", "sentence2"],
+        "qqp": ["question1", "question2"],
+        "stsb": ["sentence1", "sentence2"],
+        "mnli": ["premise", "hypothesis"],
+        "qnli": ["question", "sentence"],
+        "rte": ["sentence1", "sentence2"],
+        "wnli": ["sentence1", "sentence2"],
+        "ax": ["premise", "hypothesis"],
+    }
+
+    glue_task_num_labels = {
+        "cola": 2,
+        "sst2": 2,
+        "mrpc": 2,
+        "qqp": 2,
+        "stsb": 1,
+        "mnli": 3,
+        "qnli": 2,
+        "rte": 2,
+        "wnli": 2,
+        "ax": 3,
+    }
+
+    loader_columns = [
+        "datasets_idx",
+        "input_ids",
+        "token_type_ids",
+        "attention_mask",
+        "start_positions",
+        "end_positions",
+        "labels",
+    ]
+
+    def __init__(
+        self,
+        model_name_or_path: str,
+        plugin: DPPluginBase,
+        task_name: str = "mrpc",
+        max_seq_length: int = 128,
+        **kwargs,
+    ):
+        super().__init__()
+        self.model_name_or_path = model_name_or_path
+        self.task_name = task_name
+        self.max_seq_length = max_seq_length
+        self.plugin = plugin
+
+        self.text_fields = self.task_text_field_map[task_name]
+        self.num_labels = self.glue_task_num_labels[task_name]
+        self.tokenizer: PreTrainedTokenizer = AutoTokenizer.from_pretrained(
+            self.model_name_or_path, use_fast=True
+        )
+        self.setup()
+
+    def setup(self):
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+        self.dataset = datasets.load_dataset("glue", self.task_name)
+
+        for split in self.dataset.keys():
+            self.dataset[split] = self.dataset[split].map(
+                self.convert_to_features,
+                batched=True,
+                remove_columns=["label"],
+            )
+            self.columns = [
+                c for c in self.dataset[split].column_names if c in self.loader_columns
+            ]
+            self.dataset[split].set_format(type="torch", columns=self.columns)
+
+        self.eval_splits = [x for x in self.dataset.keys() if "validation" in x]
+
+    def prepare_data(self):
+        datasets.load_dataset("glue", self.task_name)
+        AutoTokenizer.from_pretrained(self.model_name_or_path, use_fast=True)
+
+    def train_dataloader(self):
+        return self.plugin.prepare_dataloader(
+            self.dataset["train"],
+            shuffle=True,
+            drop_last=True,
+        )
+
+    def convert_to_features(self, example_batch):
+        # Either encode single sentence or sentence pairs
+        if len(self.text_fields) > 1:
+            texts_or_text_pairs = list(
+                zip(
+                    example_batch[self.text_fields[0]],
+                    example_batch[self.text_fields[1]],
+                )
+            )
+        else:
+            texts_or_text_pairs = example_batch[self.text_fields[0]]
+
+        # Tokenize the text/text pairs
+        features = self.tokenizer.batch_encode_plus(
+            texts_or_text_pairs,
+            max_length=self.max_seq_length,
+            padding="max_length",
+            truncation=True,
+        )
+
+        # Rename label to labels to make it easier to pass to model forward
+        features["labels"] = example_batch["label"]
+
+        return features
